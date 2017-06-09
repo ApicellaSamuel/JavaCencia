6b98da4fc86b03ad52a827b3aa9c552cd16777c7
sam <samuelapicella@hotmail.com>
2017-06-09 11:09:59 +0200

    Ripasso esame

---
+++ README.md
@@ -0,0 +1,12 @@
+#### Prima lezione
+I tipi di dato primitivi
+#### Seconda lezione
+Le funzioni
+#### Terza lezione
+Le eccezzioni
+#### Quarta lezione
+Le liste
+
+
+
+###### ```mancano 17 lezioni``` 
---
+++ src/lections/mainLections.java
@@ -0,0 +1,10 @@
+package lections;
+
+public class mainLections {
+    public static void main(String[] args){
+        double a = 45;
+        int b = 2;
+        double c = a / b;
+        System.out.println(c);
+    }
+}
